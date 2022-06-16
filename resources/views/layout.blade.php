4718a3b224cbd582003e7a88db01750f812d657c
tran-ba-quang <tranbaquangoooo@oooogmailcom>
2022-06-16 23:07:50 +0700

    first commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,12 +1,114 @@
 @extends('pages.home')
 @section('header')
+<nav class="navbar" style="box-shadow:none;">
+  <div class="container-fluid">
+    <a class="navbar-brand">Văn phòng điện tử</a>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<nav class="navbar navbar-expand-lg"  style="box-shadow:none;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img id="logo" src="https://smc.vn/wp-content/uploads/2019/12/logo.png" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+    aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">TRANG CHỦ</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          GIỚI THIỆU
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">THÔNG ĐIỆP TỪ CHỦ TỊCH</a></li>
+            <li><a class="dropdown-item" href="#">GIỚI THIỆU CHUNG</a></li>
+            <li><a class="dropdown-item" href="#">LỊCH SỬ PHÁT TRIỂN</a></li>
+            <li><a class="dropdown-item" href="#">BAN LÃNH ĐẠO</a></li>
+            <li><a class="dropdown-item" href="#">CÔNG TY THÀNH VIÊN</a></li>        
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            LĨNH VỰC HOẠT ĐỘNG 
+          </a>
+          <ul class="dropdown-menu">
+            <li><span class="dropdown-item-text">THƯƠNG MẠI</span></li>
+            <li><a class="dropdown-item" href="#">Thép Xây Dựng</a></li>
+            <li><a class="dropdown-item" href="#"> Thép Tấm Lá</a></li>
+            <li><a class="dropdown-item" href="#">Thép Hình, Xà Gồ</a></li>
+            <li><a class="dropdown-item" href="#">Các Dự Án Cung Cấp Thép</a></li>
+
+            <li><span class="dropdown-item-text">COIL CENTER</span></li>
+            <li><a class="dropdown-item" href="#">Gia Công Cắt, Chặt, Xả Băng Thép Tấm Cán Nóng</a></li>
+            <li><a class="dropdown-item" href="#">Gia Công Cắt, Chặt, Xả Băng Thép Lá Cán Nguội</a></li>
+
+            <li><span class="dropdown-item-text">SẢN XUẤT</span></li>
+            <li><a class="dropdown-item" href="#">Thép Lá Nguội, Thép Lá Mạ Kẽm</a></li>
+            <li><a class="dropdown-item" href="#">Ống Thép</a></li>
+            <li><a class="dropdown-item" href="#">Lưới Thép Hàn</a></li>
+        </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          TRUYỀN THÔNG
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">TIN DOANH NGHIỆP</a></li>
+            <li><a class="dropdown-item" href="#">TIN TỨC KHÁC</a></li>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">THƯ VIỆN
+                <li><a class="dropdown-item" href="#">HÌNH ẢNH</a></li>
+                <li><a class="dropdown-item" href="#">PHIM TƯ LIỆU</a></li>
+            </ul>      
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          QUAN HỆ CỔ ĐÔNG
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">CÔNG BỐ THÔNG TIN</a></li>
+            <li><a class="dropdown-item" href="#">BÁO CÁO TÀI CHÍNH</a></li>
+            <li><a class="dropdown-item" href="#">ĐẠI HỘI CỔ ĐÔNG</a></li>
+            <li><a class="dropdown-item" href="#">BÁO CÁO THƯỜNG NIÊN</a></li>
+            <li><a class="dropdown-item" href="#">QUẢN TRỊ CÔNG TY</a></li>      
+            <li><a class="dropdown-item" href="#">THÔNG TIN CỔ PHIẾU</a></li>  
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">HỒI KÝ SMC</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          TUYỂN DỤNG
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">LÝ DO CHỌN SMC</a></li>
+            <li><a class="dropdown-item" href="#">CƠ HỘI NGHỀ NGHIỆP</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">LIÊN HỆ</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+@endsection
+@section('header')
     <div class="page-wrapper">
         <div class="header-wrapper">
             <header id="header" class="header-builder">
                 <div class="header-gr1">
                     <div class="header-top">
                         <div class="header-row">
-                            <div class="header-row">
                                 <div class="header-col header-left">
                                     <ul class="custom-menu">
                                         <li>
@@ -20,7 +122,7 @@
                                         <input type="search" id="search" placeholder="Search..." />
                                     </div>
                                 </div>
-                            </div>
+                            
                         </div>
                     </div>
                     <!--trang chu-->
@@ -196,4 +298,116 @@
             </header>
         </div>
     </div>
+@endsection
+
+@section('footer')
+<!-- Footer -->
+<footer class="text-center text-lg-start  text-muted"  style="background-color:#005da3 ; color: #f5f5f5!important;">
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            THÔNG TIN LIÊN HỆ
+          </h6>
+          <p><i class="fas fa-home me-3"></i> 396 Ung Văn Khiêm, Phường 25, Quận Bình Thạnh, TP. Hồ Chí Minh.</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            tranbaquangoooo@gmail.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            GIỚI THIỆU
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Thông Điệp Từ Chủ Tịch</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Giới Thiệu Chung</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Lịch Sử Phát Triển</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Ban Lãnh Đạo</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Công Ty Thành Viên</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            TRUYỀN THÔNG
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Tin Doanh Nghiệp</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Tin Tức Khác</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Hình Ảnh</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Phim Tư Liệu</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+        
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            QUAN HỆ CỔ ĐÔNG
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Công Bố Thông Tin</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Báo Cáo Tài Chính</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Đại Hội Cổ Đông</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Báo Cáo Thường Niên</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Quản Trị Công Ty</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Thông Tin Cổ Phiếu</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+        
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color:#004e8b;">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,21 +9,39 @@
     <link rel="stylesheet" href="{{asset('public/css/slider.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="shortcut icon" href="{{asset('public/img/logo.png')}}" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+      rel="stylesheet"
+    />
+    <!-- Google Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      rel="stylesheet"
+    />
+    <!-- MDB -->
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.2.0/mdb.min.css"
+      rel="stylesheet"
+    />
+
 
     <title>Công Ty Cổ Phần Đầu Tư Thương Mại SMC - Nhà Phân Phối Thép Chuyên Nghiệp</title>
 </head>
 <body>
     <div class="page-wrapper">
-        <div class="header-wrapper">
+        <div class="header-wrapper" >
             <header id="header" class="header-builder">
               @yield("header")
             </header>
         </div>
-
         <div class="main">
-          <div class="container">
+          <div class="container2">
             <div class="page-content">
-              <div class="slider">
+              <div class="slider" data-aos="fade-left">
                 <div class="slides">
                   <input type="radio" name="radio-btn" id="radio1">
                   <input type="radio" name="radio-btn" id="radio2">
@@ -45,7 +63,7 @@
                 </div>
               </div>
 
-              <div class="home-intro">
+              <div class="home-intro" data-aos="zoom-in-left">
                 <div class="box-title">
                   <div class="box-title-name">
                     Công Ty Cổ Phần Đầu Tư Thương Mại SMC
@@ -73,29 +91,203 @@
                   </div>
               </div>
               
-              <div class="home-services">
+              <div class="home-services" data-aos="fade-up"
+              data-aos-anchor-placement="center-bottom">
                 <div class="services-top">
                   <div class="services-top-name">
                     <h3>LĨNH VỰC HOẠT ĐỘNG</h3>
                   </div>
                 </div>
+                  <div class="d-flex justify-content-center">
+                    <div class="main-services-1"><img src="https://smc.vn/wp-content/uploads/2020/04/4af43043c9f032ae6be1.jpg" alt=""></div>
+                    <div class="main-services-2"><img src="https://smc.vn/wp-content/uploads/2020/04/4af43043c9f032ae6be1.jpg" alt=""></div>
+                    <div class="main-services-3"><img src="https://smc.vn/wp-content/uploads/2020/04/4af43043c9f032ae6be1.jpg" alt=""></div>
+                  </div>
+              </div>
 
-                  <div class="main-services">
-                    <div class="main-services-1">
-                      <img src="https://smc.vn/wp-content/uploads/2020/04/4af43043c9f032ae6be1.jpg" alt="">
+              <div class="the-media"  data-aos="fade-up">
+                <div class="box-title">
+                    <div class="box-title-name">
+                      TRUYỀN THÔNG
                     </div>
-                    <div class="main-services-2">
-                      <img src="https://smc.vn/wp-content/uploads/2020/04/4af43043c9f032ae6be1.jpg" alt="">
-                    </div>
-                    <div class="main-services-3">
-                      <img src="https://smc.vn/wp-content/uploads/2020/04/4af43043c9f032ae6be1.jpg" alt="">
+                </div>
+                <div class="main-the-media">
+                    <div class="content-the-media">
+                        <div class="post-media">
+                          <div class="post-image">
+                            <img src="https://smc.vn/wp-content/uploads/2020/04/4af43043c9f032ae6be1.jpg" alt="">
+                          </div>                         
+                          <div class="post-item-content">
+                            <h3>tin tức nóng...</h3>
+                          </div>                           
+                        </div>
+
+                        <div class=" post-media">
+                          <div class="post-image">
+                            <img src="https://smc.vn/wp-content/uploads/2020/04/4af43043c9f032ae6be1.jpg" alt="">
+                          </div>                         
+                          <div class="post-item-content">
+                            <h3>tin tức nóng...</h3>
+                          </div>                           
+                        </div>
+
+                        <div class="post-media">
+                          <div class="post-image">
+                            <img src="https://smc.vn/wp-content/uploads/2020/04/4af43043c9f032ae6be1.jpg" alt="">
+                          </div>                         
+                          <div class="post-item-content">
+                            <h3>tin tức nóng...</h3>
+                          </div>                           
+                        </div>
+
+                        <div class=" post-media">
+                          <div class="post-image">
+                            <img src="https://smc.vn/wp-content/uploads/2020/04/4af43043c9f032ae6be1.jpg" alt="">
+                          </div>                         
+                          <div class="post-item-content">
+                            <h3>tin tức nóng...</h3>
+                          </div>                           
+                        </div>
+
+                        <div class="post-media">
+                          <div class="post-image">
+                            <img src="https://smc.vn/wp-content/uploads/2020/04/4af43043c9f032ae6be1.jpg" alt="">
+                          </div>                         
+                          <div class="post-item-content">
+                            <h3>tin tức nóng...</h3>
+                          </div>                           
+                        </div>
+
+                        <div class=" post-media">
+                          <div class="post-image">
+                            <img src="https://smc.vn/wp-content/uploads/2020/04/4af43043c9f032ae6be1.jpg" alt="">
+                          </div>                         
+                          <div class="post-item-content">
+                            <h3>tin tức nóng...</h3>
+                          </div>                           
+                        </div>
                     </div>
                 </div>
               </div>
 
-              
+              <div class="partner" data-aos="fade-right"
+              data-aos-offset="300"
+              data-aos-easing="ease-in-sine">
+                  <div class="box-title">
+                      <div class="box-title-name">
+                        ĐỐI TÁC
+                      </div>
+                  </div>
+                  <div class="text-partner">
+                      <p>Là thương hiệu hàng đầu trên lĩnh vực phân phối, hoạt động gia công và sản xuất kinh doanh thép.
+                        Công ty Cổ Phần Đầu Tư Thương Mại SMC luôn được các đối tác đánh giá cao về năng lực và uy tín kinh doanh. 
+                        SMC đã trở thành đối tác tin cậy của hàng loạt công ty, tập đoàn hàng đầu trong rất nhiều lĩnh vực.
+                      </p>
+                  </div>
+                  <div class="main-partner">
+                      <div class="logo-partner">
+                        <div class="post-logo-partner" >
+                            <div class="post-image-logo">
+                              <img src="https://smc.vn/wp-content/uploads/2020/04/Posco.png" alt="">
+                            </div>                             
+                        </div>
+
+                        <div class="post-logo-partner" >
+                            <div class="post-image-logo">
+                              <img src="https://smc.vn/wp-content/uploads/2020/04/Posco.png" alt="">
+                            </div>                             
+                        </div>
+
+                        <div class="post-logo-partner" >
+                            <div class="post-image-logo">
+                              <img src="https://smc.vn/wp-content/uploads/2020/04/Posco.png" alt="">
+                            </div>                             
+                        </div>
+
+                        <div class="post-logo-partner" >
+                            <div class="post-image-logo">
+                              <img src="https://smc.vn/wp-content/uploads/2020/04/Posco.png" alt="">
+                            </div>                             
+                        </div>
+
+                        <div class="post-logo-partner" >
+                            <div class="post-image-logo">
+                              <img src="https://smc.vn/wp-content/uploads/2020/04/Posco.png" alt="">
+                            </div>                             
+                        </div>
+
+                        <div class="post-logo-partner" >
+                            <div class="post-image-logo">
+                              <img src="https://smc.vn/wp-content/uploads/2020/04/Posco.png" alt="">
+                            </div>                             
+                        </div>
+
+                        <div class="post-logo-partner" >
+                            <div class="post-image-logo">
+                              <img src="https://smc.vn/wp-content/uploads/2020/04/Posco.png" alt="">
+                            </div>                             
+                        </div>
+
+                        <div class="post-logo-partner" >
+                            <div class="post-image-logo">
+                              <img src="https://smc.vn/wp-content/uploads/2020/04/Posco.png" alt="">
+                            </div>                             
+                        </div>
+
+                        <div class="post-logo-partner" >
+                            <div class="post-image-logo">
+                              <img src="https://smc.vn/wp-content/uploads/2020/04/Posco.png" alt="">
+                            </div>                             
+                        </div>
+
+                        <div class="post-logo-partner" >
+                            <div class="post-image-logo">
+                              <img src="https://smc.vn/wp-content/uploads/2020/04/Posco.png" alt="">
+                            </div>                             
+                        </div>
+
+                        <div class="post-logo-partner" >
+                            <div class="post-image-logo">
+                              <img src="https://smc.vn/wp-content/uploads/2020/04/Posco.png" alt="">
+                            </div>                             
+                        </div>
+
+                        <div class="post-logo-partner" >
+                            <div class="post-image-logo">
+                              <img src="https://smc.vn/wp-content/uploads/2020/04/Posco.png" alt="">
+                            </div>                             
+                        </div>
+
+                        <div class="post-logo-partner" >
+                            <div class="post-image-logo">
+                              <img src="https://smc.vn/wp-content/uploads/2020/04/Posco.png" alt="">
+                            </div>                             
+                        </div>
+
+                        <div class="post-logo-partner" >
+                            <div class="post-image-logo">
+                              <img src="https://smc.vn/wp-content/uploads/2020/04/Posco.png" alt="">
+                            </div>                             
+                        </div>
+
+                        <div class="post-logo-partner" >
+                            <div class="post-image-logo">
+                              <img src="https://smc.vn/wp-content/uploads/2020/04/Posco.png" alt="">
+                            </div>                             
+                        </div>
+                      </div>
+                  </div>
+              </div>
             </div>
           </div>
+        </div>
+
+        <div class="footer-i" data-aos="fade-right"
+        data-aos-offset="300"
+        data-aos-easing="ease-in-sine">
+              <header>
+                @yield("footer")
+              </header>
         </div>
     </div>
 </body>
@@ -103,3 +295,14 @@
 
 <script src="{{asset('public/js/banner.js')}}"></script>
 <script src="{{asset('public/js/header.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+    AOS.init();
+</script>
+<!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.2.0/mdb.min.js"
+></script>
